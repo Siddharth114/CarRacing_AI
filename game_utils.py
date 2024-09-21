ebd13948f8ef3213ac45d98298ffbc7ba267b5c6
Siddharth <2004sid@gmail.com>
2024-09-21 10:36:25 +0530

    drawing the player car

--- game_utils.py
+++ game_utils.py
@@ -19,3 +19,10 @@ def scale_image(image, scale_factor):
     new_size = tuple(map(lambda x: int(x * scale_factor), current_size))
 
     return pygame.transform.scale(image, new_size)
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    # Rotating the image around the centre of the image, instead of around the top left of the image
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft = top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
+    
--- human_game.py
+++ human_game.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from game_utils import resize_images_to_largest, scale_image
+from game_utils import resize_images_to_largest, scale_image, blit_rotate_center
 
 GRASS, TRACK, TRACK_BORDER = resize_images_to_largest(
     ["assets/grass.jpg", "assets/track.png", "assets/track-border.png"]
@@ -14,21 +14,47 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Human Playable Game")
 
+
+class AbstractCar:
+    def __init__(self, max_velocity, rotation_velocity):
+        self.image = self.IMAGE
+        self.max_velocity = max_velocity
+        self.velocity = 0
+        self.rotation_velocity = rotation_velocity
+        self.angle = 0
+        self.x, self.y = self.START_POSITION
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_velocity
+        elif right:
+            self.angle -= self.rotation_velocity
+    
+    def draw(self, win):
+        blit_rotate_center(win, self.image, (self.x, self.y), self.angle)
+
+
+class HumanCar(AbstractCar):
+    IMAGE = CAR
+    START_POSITION = (180,200)
+
 running = True
 FPS = 60
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0))]
+human_car = HumanCar(4,4)
 
-def draw(win, images):
+
+def draw(win, images, player_car):
     for image, position in images:
         win.blit(image, position)
+    player_car.draw(WIN)
+    pygame.display.update()
 
 while running:
     clock.tick(FPS)
 
-    draw(WIN, images)
-
-    pygame.display.update()
+    draw(WIN, images, human_car)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
