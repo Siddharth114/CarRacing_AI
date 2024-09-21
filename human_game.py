1405f865fd20f56590778ead3c973d4fb16ea22a
Siddharth <2004sid@gmail.com>
2024-09-21 08:01:41 +0530

    loading in image assets

---
+++ human_game.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+import math
+
+GRASS = pygame.image.load('assets/grass.png')
+TRACK = pygame.image.load('assets/track.png')
+TRACK_BORDER = pygame.image.load('assets/track_border.png')
+FINISH = pygame.image.load('assets/finish.png')
+CAR = pygame.image.load('assets/red_car.png')
+
